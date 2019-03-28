0568a560d46c79a3019c978c0bfe0fae6b1aecfc
Evgeniy Kirillov <kirillov@siberian.pro>
2019-03-28 20:42:02 +0300

    start
    s

---
+++ script.js
@@ -0,0 +1,18 @@
+let btn = document.getElementById('add');
+
+btn.onclick=function CreateTask(){
+    let task ={
+    };
+
+    task.name = prompt("Название задачи");
+    task.time = prompt("Время на задачу");
+    task.assigne = prompt("Исполняющее лицо");
+    console.log(task);
+
+    let mainTable = document.getElementById("mainTable");
+    let newTask = document.createElement("tr");
+    let newSection = document.createElement("td");
+    
+    mainTable.insertRow("test").insertCell("west").textContetn=task.name;
+};    
+
