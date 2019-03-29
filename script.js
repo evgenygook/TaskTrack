1e1e1fbcdbb0315c41cfec0275d40c3026edcaba
Evgeniy Kirillov <kirillov@siberian.pro>
2019-03-29 23:37:14 +0300

    timer2

--- script.js
+++ script.js
@@ -12,29 +12,15 @@ btn.onclick=function CreateTask(){
 
     let mainTable = document.getElementById("mainTable");
     let newTask = document.createElement("tr");
-    
-    let time = new Date(),
-        h = time.getHours().toString(),
-        m = time.getMinutes().toString(),
-        s = time.getSeconds().toString();
-    
-    if (h.length <2){
-        h= "0" +h;
-    }
-    if (m.length <2){
-        m= "0" +m;
-    }
-    if (s.length <2){
-        s= "0" +s;
-    } 
-    
-    let clockString = h + ":" + m + ":" + s; 
-
 
     let newCell= mainTable.appendChild(newTask);
     newCell.insertCell(0).textContent=task.name;
     newCell.insertCell(1).textContent=task.time;
     newCell.insertCell(2).textContent=task.assigne;
-    newCell.insertCell(3).textContent=clockString;
-};    
+    newCell.insertCell(3).textContent="Здесь время";
+    };
+    
+
+
+
 
