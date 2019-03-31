04d839f4910b851d6575152dfaea599cde587e05
Evgeniy Kirillov <kirillov@siberian.pro>
2019-03-31 20:34:47 +0300

    refactor

--- script.js
+++ script.js
@@ -1,26 +1,53 @@
-let btn = document.getElementById('add');
+let task ={};
+let button = document.getElementsByTagName("input");
+let mainTable = document.getElementById("mainTable");
+let newTask = document.createElement("tr");
 
+console.log(button); 
 
-
-btn.onclick=function CreateTask(){
-    let task ={};
+function CreateTask(){
 
     task.name = prompt("Название задачи");
     task.time = prompt("Время на задачу");
     task.assigne = prompt("Исполняющее лицо");
     console.log(task);
+    
 
-    let mainTable = document.getElementById("mainTable");
-    let newTask = document.createElement("tr");
-
+    
     let newCell= mainTable.appendChild(newTask);
     newCell.insertCell(0).textContent=task.name;
     newCell.insertCell(1).textContent=task.time;
     newCell.insertCell(2).textContent=task.assigne;
-    newCell.insertCell(3).textContent="Здесь время";
-    };
-    
+    newCell.insertCell(3).textContent=clockString;
+    newCell.insertCell(4).innerHTML="<input type='button' value='Завершить'>";
+       
+};
 
+function createTime(){
 
+    let time = new Date(),
+    h = time.getHours().toString(),
+    m = time.getMinutes().toString(),
+    s = time.getSeconds().toString();
 
+    if (h.length <2){
+    h= "0" +h;
+    }
+    if (m.length <2){
+    m= "0" +m;
+    }
+    if (s.length <2){
+    s= "0" +s;
+    } 
 
+    return clockString = h + ":" + m + ":" + s;
+}
+
+button[0].addEventListener("click", function(event){
+    createTime();
+    CreateTask();
+})
+
+function endTime(){
+    this.time = null;
+}
